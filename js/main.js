fede46494eb033cf3399c107c0c9d0c00c85f5dd
Fallet Steve <steve.fallet@divtec.ch>
2022-09-06 17:02:54 +0200

    ajout animations

--- js/main.js
+++ js/main.js
@@ -5,8 +5,22 @@
 
 //Récupération de la liste déroulante
 const listeBackground = document.getElementById('background');
-
+// Changer le background lors du changement de valeur dans la listre déroulante
 listeBackground.addEventListener('change', function () {
-  const image = listeBackground.value
-  document.body.style.backgroundImage = `url(../img/backgounds/${image})`
+    const image = listeBackground.value
+    document.body.style.backgroundImage = `url(../img/backgounds/${image})`
+})
+
+const formulaire = document.querySelector('form');
+formulaire.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const btCreer = document.querySelector('[type="submit"]');
+    btCreer.classList.add('animate__heartBeat');
+
+    setTimeout(function () {
+        document.getElementById('dialog').style.display = 'block';
+        formulaire.reset()
+        btCreer.classList.remove('animate__heartBeat');
+    }, 1000)
+
 })
