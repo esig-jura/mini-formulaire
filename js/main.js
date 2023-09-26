f89b4d22ae77e403f238f663d61bbe76bdb2d155
Steve Fallet <steve.fallet@gmail.com>
2023-09-26 14:06:38 +0200

    refactoring

--- js/main.js
+++ js/main.js
@@ -2,30 +2,31 @@
  * @author Steve Fallet
  * @since 2023.09.26
  */
-//Récupération de la liste déroulante
-const listeBackground = document.getElementById('background');
 
+/*****************************************
+ CHANGEMENT DE L'IMAGE D'ARRIÈRE-PLAN
+ *****************************************/
+// Récupération du <body>
+const body = document.querySelector('body');
+// Récupération de la liste déroulante
+const listeBackground = document.getElementById('background');
 // Écoute le changement de sélection "change" de la liste déroulante
-// Et change l'arrière-plan du body
 listeBackground.addEventListener('change', function () {
-  const image = listeBackground.value
-  document.body.style.backgroundImage = `url(./img/backgrounds/${image})`
+  body.style.backgroundImage = `url(./img/backgrounds/${listeBackground.value})`
 })
 
 /*****************************************
  ENVOI DU FORMULAIRE
  *****************************************/
 
-// Récupération du formulaire et de ses éléments
+// Récupération du formulaire et de champs de mot de passe
 const formulaire = document.querySelector('form');
 const txtPassword = document.querySelector('#password');
 const txtPasswordConfirm = document.querySelector('#password-confirm');
-
 // Ecoute l'envoi du formulaire
 formulaire.addEventListener('submit', function (event) {
   // Annule l'envoi du formulaire
   event.preventDefault();
-
   // Si longueur (length) du mot de passes plus petit que 3
   if (txtPassword.value.length < 3) {
     alert('Le mot de passe doit contenir au moins 3 caractères !');
@@ -36,7 +37,6 @@ formulaire.addEventListener('submit', function (event) {
     alert('Les mots de passe ne correspondent pas !');
     return; // Arrête la fonction
   }
-
   // Si tout est OK, on envoie le formulaire
   alert('Compte créé avec succès !');
   formulaire.submit()
